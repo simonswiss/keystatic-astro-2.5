788bec307aa55b385d679388d867323453e6fa0b
Simon Vrachliotis <simon@simonswiss.com>
2023-06-02 13:40:36 +1000

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'hybrid',
   experimental: {
-    hybridOutput: true,
+    hybridOutput: true
   },
-})
+  adapter: vercel()
+});
